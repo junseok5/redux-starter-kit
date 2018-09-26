8ea37423078e551e34a7d15d6e5ae8361cf99c90
junseok5 <vkehrkrj@naver.com>
2018-09-27 01:44:48 +0900

    completed project

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# redux-starter-kit
+redux-starter-kit
+===================
+redux project to study
 
-리덕스 스타터킷으로 사용되는 프로젝트입니다.
-[Ducks 구조](https://velopert.com/3358) 가 적용된 카운터 코드가 안에 들어있습니다.
+## Stack
+* react
+* redux
+* react-redux
+* redux-logger
+* redux-pender
+* axios
---
+++ src/modules/post.js
@@ -0,0 +1,45 @@
+import { handleActions, createAction } from 'redux-actions'
+import { pender, applyPenders } from 'redux-pender'
+import axios from 'axios'
+
+function getPostAPI(postId) {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+}
+
+const GET_POST = 'GET_POST'
+
+export const getPost = createAction(GET_POST, getPostAPI)
+
+const initialState = {
+    data: {
+        title: '',
+        body: ''
+    }
+}
+
+const reducer = handleActions({
+    // 다른 일반 액션들을 관리...
+}, initialState)
+
+export default applyPenders(reducer, [
+    {
+        type: GET_POST,
+        onSuccess: (state, action) => {
+            const { title, body }= action.payload.data
+            return {
+                data: {
+                    title,
+                    body
+                }
+            }
+        },
+        onCancel: (state, action) => {
+            return {
+                data: {
+                    title: '취소됨',
+                    body: '취소됨'
+                }
+            }
+        }
+    }
+])
